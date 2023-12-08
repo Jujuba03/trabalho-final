cfec88bc0d9fe1e37d526a172012f14928a4b74c
Jujuba03 <145704882+Jujuba03@users.noreply.github.com>
2023-12-07 22:17:35 -0300

    5

--- index.php
+++ index.php
@@ -9,28 +9,9 @@
 <body>
 
 <?php
-session_start();
-
-if (isset($_SESSION['usuario_id'])) {
-    echo "<h1>Bem-vindo, " . $_SESSION['usuario_nome'] . "!</h1>";
-    echo "<p><a href='logout.php'>Logout</a></p>";
-    // Adicione um botão para redirecionar para a pasta cred1
-    echo "<button onclick=\"redirecionarParaCred1()\">Ir para cred1</button>";
-} else {
-    echo "<h1>CRUD de Usuários</h1>";
-    echo "<ul>
-            <li><a href='listar_usuarios.php'>Listar Usuários</a></li>
-            <li><a href='cadastro.php'>Cadastrar Usuário</a></li>
-            <li><a href='login.php'>Login</a></li><br>
-            <li><a href='listar_Brincos.php'>Listar Brincos</a></li><br>
-            <li><a href='cadastroB.php'>Cadastrar Usuário</a></li>
-          </ul>";
-}
-
-// Adicione um script JavaScript para redirecionar para a pasta cred1
 echo "<script>
-        function redirecionarParaCred1() {
-            window.location.href = 'cred1';
+        function redirecionarParaCrud1() {
+            window.location.href = 'crud1';
         }
       </script>";
 ?>
