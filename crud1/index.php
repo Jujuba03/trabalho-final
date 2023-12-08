b1e115e1faf86daccb46546e7d015a9ee602da70
Jujuba03 <145704882+Jujuba03@users.noreply.github.com>
2023-12-07 22:19:39 -0300

    3

---
+++ crud1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUDs</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+echo "<script>
+        function redirecionarParaCrud1() {
+            window.location.href = 'crud1';
+        }
+      </script>";
+?>
+
+</body>
+</html>
